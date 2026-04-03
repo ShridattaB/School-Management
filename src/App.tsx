61ea6a667bd34dfbcc269db669089375c2d203cf
ShridattaB <shridattabhimanavaru@gmail.com>
2026-04-03 11:18:47 +0530

    run tha cammand of eslint and priter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { useState } from 'react';
 
 function App() {
- 
+  const [lint, useLint] = useState('');
+  let name = 'shridatt';
 
   return (
-  <>
-  <h1>School Management System</h1>
-  </>
+    <>
+      <h1>School Management System</h1>
+      <p>
+        this
+        <>Pjhdf</>
+      </p>
+    </>
   );
 }
 
